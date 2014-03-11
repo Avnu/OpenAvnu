9836be9cf94bb14a7f64900a6144450e0f16c6b5
Carolyn Wyborny <carolyn.wyborny@intel.com>
2014-03-11 06:15:37 +0000

    igb: Add register defines needed for time sync functions
    This patch adds defines needed for implementing the auxiliary time sync
    functions and also changes code to call the updated defines instead of
    the old.
    
    Reported-by: Richard Cochran <ricahrdcochran@gmail.com>
    Signed-off-by: Carolyn Wyborny <carolyn.wyborny@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Roland Hii <roland.king.guan.hii@intel.com>

--- kmod/igb/igb_ptp.c
+++ kmod/igb/igb_ptp.c
@@ -972,7 +972,7 @@ void igb_ptp_init(struct igb_adapter *adapter)
 
 	/* Initialize the time sync interrupts for devices that support it. */
 	if (hw->mac.type >= e1000_82580) {
-		E1000_WRITE_REG(hw, E1000_TSIM, E1000_TSIM_TXTS);
+		E1000_WRITE_REG(hw, E1000_TSIM, TSYNC_INTERRUPTS);
 		E1000_WRITE_REG(hw, E1000_IMS, E1000_IMS_TS);
 	}
 
@@ -1058,7 +1058,7 @@ void igb_ptp_reset(struct igb_adapter *adapter)
 	case e1000_i211:
 		/* Enable the timer functions and interrupts. */
 		E1000_WRITE_REG(hw, E1000_TSAUXC, 0x0);
-		E1000_WRITE_REG(hw, E1000_TSIM, E1000_TSIM_TXTS);
+		E1000_WRITE_REG(hw, E1000_TSIM, TSYNC_INTERRUPTS);
 		E1000_WRITE_REG(hw, E1000_IMS, E1000_IMS_TS);
 		break;
 	default:
