d8c431a9ab55aa9d26ebdb86778f77f5688d650a
Andrew Elder <aelder@audioscience.com>
2014-05-23 17:44:08 -0400

    mrp: examples: mrpdhelper: add MSRP TalkerFailed codes

--- examples/mrp_client/mrpdhelper.h
+++ examples/mrp_client/mrpdhelper.h
@@ -38,6 +38,28 @@
 This module provides helper functions for communicating with the MRPD daemon.
 ***************************************************************************/
 
+enum mrpdhelper_talkerfailed_codes {
+	mrpdhelper_talkerfailed_success,
+	mrpdhelper_talkerfailed_bandwidth,
+	mrpdhelper_talkerfailed_bridge_resources,
+	mrpdhelper_talkerfailed_bridge_class_bandwidth,
+	mrpdhelper_talkerfailed_id_in_use,
+	mrpdhelper_talkerfailed_preempted,
+	mrpdhelper_talkerfailed_latency_change,
+	mrpdhelper_talkerfailed_egress_port_not_ascapable,
+	mrpdhelper_talkerfailed_illegal_destination_address,
+	mrpdhelper_talkerfailed_no_msrp_resources,
+	mrpdhelper_talkerfailed_no_mmrp_resources,
+	mrpdhelper_talkerfailed_destination_address_storage,
+	mrpdhelper_talkerfailed_illegal_priority,
+	mrpdhelper_talkerfailed_max_framesize,
+	mrpdhelper_talkerfailed_msrp_fan_in_limit,
+	mrpdhelper_talkerfailed_id_change,
+	mrpdhelper_talkerfailed_vlan_blocked,
+	mrpdhelper_talkerfailed_vlan_tagging_disabled,
+	mrpdhelper_talkerfailed_class_priority_mismatch
+};
+
 enum mrpdhelper_attribtype {
 	mrpdhelper_attribtype_null,
 	mrpdhelper_attribtype_mmrp,
