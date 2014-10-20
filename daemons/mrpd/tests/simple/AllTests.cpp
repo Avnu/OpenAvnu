6759b6957476d5164763f2e1486a1590a8533330
Andrew Elder <aelder@audioscience.com>
2014-10-20 12:55:24 -0400

    mrpd: cpputest heading corrections in the copyright section

--- daemons/mrpd/tests/simple/AllTests.cpp
+++ daemons/mrpd/tests/simple/AllTests.cpp
@@ -13,7 +13,7 @@
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
 
-   3. Neither the name of the Intel Corporation nor the names of its
+   3. Neither the name of the AudioScience, Inc nor the names of its
       contributors may be used to endorse or promote products derived from
       this software without specific prior written permission.
 
