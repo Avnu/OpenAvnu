5f1deeccfb90ea8c19a8edd1765385b71bc3eafa
Andrew Elder <aelder@audioscience.com>
2014-10-22 16:35:10 -0400

    mrp: cpputests, add mmrp and mvrp tests

---
+++ daemons/mrpd/tests/simple/mmrp_tests.cpp
@@ -0,0 +1,104 @@
+/******************************************************************************
+
+  Copyright (c) 2014, AudioScience, Inc.
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+   3. Neither the name of the AudioScience, Inc nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#ifdef __linux__
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#else
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#define PRIu64       "I64u"
+#define PRIx64       "I64x"
+#endif
+
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+
+#include "mrpd.h"
+#include "mrp.h"
+#include "mmrp.h"
+#include "parse.h"
+
+    extern struct mvrp_database *MMRP_db;
+
+}
+
+#define STREAM_DA                "010203040506"
+
+static struct sockaddr_in client;
+
+TEST_GROUP(MmrpTestGroup)
+{
+    void setup()
+    {
+        mmrp_init(1);
+    }
+
+    void teardown()
+    {
+        mmrp_reset();
+        mrpd_reset();
+    }
+};
+
+TEST(MmrpTestGroup, RegisterAttrib)
+{
+    struct mmrp_attribute a_ref;
+    struct mmrp_attribute *a_mmrp = NULL;
+	int i;
+    int err_index = 0;
+    int parse_status = 0;
+	char cmd_string[] = "M++:M=" STREAM_DA;
+
+    CHECK(MMRP_db != NULL);
+
+    /* here we fill in a_ref struct with target values */
+	a_ref.type = MMRP_MACVEC_TYPE;
+	for (i = 0; i < 6; i++)
+		a_ref.attribute.macaddr[i] = i + 1; /* matches defn of STREAM_DA */
+
+    /* use string interface to get MMRP to create attrib in it's database */
+    mmrp_recv_cmd(cmd_string, sizeof(cmd_string), &client);
+
+    /* lookup the created attrib */
+    a_mmrp = mmrp_lookup(&a_ref);
+    CHECK(a_mmrp != NULL);
+}
---
+++ daemons/mrpd/tests/simple/mvrp_tests.cpp
@@ -0,0 +1,99 @@
+/******************************************************************************
+
+  Copyright (c) 2014, AudioScience, Inc.
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+   3. Neither the name of the AudioScience, Inc nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#ifdef __linux__
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#else
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#define PRIu64       "I64u"
+#define PRIx64       "I64x"
+#endif
+
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+
+#include "mrpd.h"
+#include "mrp.h"
+#include "mvrp.h"
+#include "parse.h"
+
+    extern struct mvrp_database *MVRP_db;
+
+}
+
+static struct sockaddr_in client;
+
+TEST_GROUP(MvrpTestGroup)
+{
+    void setup()
+    {
+        mvrp_init(1);
+    }
+
+    void teardown()
+    {
+        mvrp_reset();
+        mrpd_reset();
+    }
+};
+
+TEST(MvrpTestGroup, RegisterVLAN)
+{
+    struct mvrp_attribute a_ref;
+    struct mvrp_attribute *a_mvrp = NULL;
+    int err_index = 0;
+    int parse_status = 0;
+	char cmd_string[] = "V++:I=1234";
+
+    CHECK(MVRP_db != NULL);
+
+    /* here we fill in a_ref struct with target values */
+	a_ref.attribute = 0x1234;
+
+    /* use string interface to get MSRP to create TalkerAdv attrib in it's database */
+    mvrp_recv_cmd(cmd_string, sizeof(cmd_string), &client);
+
+    /* lookup the created attrib */
+    a_mvrp = mvrp_lookup(&a_ref);
+    CHECK(a_mvrp != NULL);
+}
