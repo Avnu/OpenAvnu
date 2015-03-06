083e817f50029531ed8dab97170812791c310ce9
Andrew Elder <aelder@audioscience.com>
2015-03-05 21:23:57 -0500

    MRP: cpputest, detect error in "I-S" operation
    When a stream is remove from the interesting list if should remove at most
    one matching TalkerAdvertise or TalkerFailed from the MSRP attribute database

--- daemons/mrpd/tests/simple/msrp_pruning_tests.cpp
+++ daemons/mrpd/tests/simple/msrp_pruning_tests.cpp
@@ -441,6 +441,8 @@ TEST(MsrpPruningTestGroup, Prune_Uninteresting_Disable_With_TA)
 	uint64_t id = 0x000fd70023580001; /* see pkt2 at top of this file */
 	char cmd_string[128];
 	int tx_flag_count = 0;
+	int ta_count_before = 0;
+	int ta_count_after = 0;
 	int rv;
 
 	/* mark as interesting */
@@ -448,6 +450,10 @@ TEST(MsrpPruningTestGroup, Prune_Uninteresting_Disable_With_TA)
 	msrp_recv_cmd(cmd_string, strlen(cmd_string) + 1, &client);
 	CHECK(msrp_tests_cmd_ok(test_state.ctl_msg_data));
 	LONGS_EQUAL(1, msrp_interesting_id_count());
+	snprintf(cmd_string, sizeof(cmd_string), "I+S:S=%" PRIx64, id + 1);
+	msrp_recv_cmd(cmd_string, strlen(cmd_string) + 1, &client);
+	CHECK(msrp_tests_cmd_ok(test_state.ctl_msg_data));
+	LONGS_EQUAL(2, msrp_interesting_id_count());
 
 	/* here we fill in a_ref struct with target values */
 	eui64_write(a_ref.attribute.talk_listen.StreamID, id);
@@ -467,15 +473,21 @@ TEST(MsrpPruningTestGroup, Prune_Uninteresting_Disable_With_TA)
 	 * At this point there is an interesting ID in the ID database and a TA in the
 	 * MSRP database.
 	 */
+	ta_count_before = msrp_count_type(MSRP_TALKER_ADV_TYPE);
 	snprintf(cmd_string, sizeof(cmd_string), "I-S:S=%" PRIx64, id);
 	msrp_recv_cmd(cmd_string, strlen(cmd_string) + 1, &client);
 	CHECK(msrp_tests_cmd_ok(test_state.ctl_msg_data));
-	LONGS_EQUAL(0, msrp_interesting_id_count());
+	LONGS_EQUAL(1, msrp_interesting_id_count());
 
 	/* lookup the created attrib (it should not be present) */
 	attrib = msrp_lookup(&a_ref);
 	CHECK(attrib == NULL);
+
+	/* check TA counts */
+	ta_count_after = msrp_count_type(MSRP_TALKER_ADV_TYPE);
+	CHECK(ta_count_before - 1 == ta_count_after);
 }
+
 
 /*
  * Adding same stream twice to interesting list returns an error.
@@ -520,6 +532,8 @@ TEST(MsrpPruningTestGroup, Prune_Uninteresting_Disable_With_TA_and_Listener)
 	uint64_t id = 0x000fd70023580001; /* see pkt2 at top of this file */
 	char cmd_string[128];
 	int tx_flag_count = 0;
+	int ta_count_before = 0;
+	int ta_count_after = 0;
 	int rv;
 
 	/* mark as interesting */
@@ -554,12 +568,17 @@ TEST(MsrpPruningTestGroup, Prune_Uninteresting_Disable_With_TA_and_Listener)
 	 */
 
 	/* mark as uninteresting */
+	ta_count_before = msrp_count_type(MSRP_TALKER_ADV_TYPE);
 	snprintf(cmd_string, sizeof(cmd_string), "I-S:S=%" PRIx64, id);
 	msrp_recv_cmd(cmd_string, strlen(cmd_string) + 1, &client);
 	CHECK(msrp_tests_cmd_ok(test_state.ctl_msg_data));
 
 	/* interesting database should be empty */
 	LONGS_EQUAL(0, msrp_interesting_id_count());
+
+	/* TA should remain the same */
+	ta_count_after = msrp_count_type(MSRP_TALKER_ADV_TYPE);
+	CHECK(ta_count_before  == ta_count_after);
 
 	/* TA should still be listed */
 	attrib = msrp_lookup(&a_ref);
