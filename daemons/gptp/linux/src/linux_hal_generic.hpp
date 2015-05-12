9d74409e80df79ceaff79783b57d756e89267744
David Cemin <david.cemin@coveloz.com>
2015-05-12 18:04:46 -0400

    [gPTP doc] linux_hal_generic.hpp doc

--- daemons/gptp/linux/src/linux_hal_generic.hpp
+++ daemons/gptp/linux/src/linux_hal_generic.hpp
@@ -62,26 +62,91 @@ private:
 #endif
 	
 public:
+	/**
+	 * Default constructor. Initializes internal variables
+	 */
 	LinuxTimestamperGeneric();
+
+	/**
+	 * @brief Resets frequency adjustment value to zero and call
+	 * linux system calls for frequency adjustment.
+	 * @return TRUE if success, FALSE if error.
+	 */
 	bool resetFrequencyAdjustment();
+
+	/**
+	 * @brief  Calls linux system call for adjusting frequency or phase.
+	 * @param  tmx [in] Void pointer that must be cast (and filled in correctly) to
+	 * the struct timex
+	 * @return TRUE if ok, FALSE if error.
+	 */
 	bool Adjust( void *tmx );
+
+	/**
+	 * @brief  Initializes the Hardware timestamp interface
+	 * @param  iface_label [in] Network interface label (used to find the phc index)
+	 * @param  iface [in] Network interface
+	 * @return FALSE in case of error, TRUE if success.
+	 */
 	virtual bool HWTimestamper_init
 	( InterfaceLabel *iface_label, OSNetworkInterface *iface );
 
+
+	/**
+	 * @brief  Inserts a new timestamp to the beginning of the
+	 * RX timestamp list.
+	 * @param tstamp [in] RX timestamp
+	 * @return void
+	 */
 	void pushRXTimestamp( Timestamp *tstamp ) {
 		tstamp->_version = version;
 		rxTimestampList.push_front(*tstamp);
 	}
+
+	/**
+	 * @brief  Post initialization procedure.
+	 * @param  ifindex struct ifreq.ifr_ifindex value
+	 * @param  sd Socket file descriptor
+	 * @param  lock [in] Instance of TicketingLock object
+	 * @return TRUE if ok. FALSE if error.
+	 */
 	bool post_init( int ifindex, int sd, TicketingLock *lock );
 
+	/**
+	 * @brief  Gets the ptp clock time information
+	 * @param  system_time [out] System time
+	 * @param  device_time [out] Device time
+	 * @param  local_clock Not Used
+	 * @param  nominal_clock_rate Not Used
+	 * @return TRUE if got the time successfully, FALSE otherwise
+	 */
 	virtual bool HWTimestamper_gettime
 	( Timestamp *system_time, Timestamp *device_time, uint32_t *local_clock,
 	  uint32_t *nominal_clock_rate );
 
+	/**
+	 * @brief  Gets the TX timestamp from hardware interface
+	 * @param  identity  Not Used
+	 * @param sequenceId Not Used
+	 * @param timestamp [out] Reference to the HW timestamp object
+	 * @param clock_value Not Used
+	 * @param last Flag that unlocks the net_lock lock when its done if is set to TRUE
+	 * @return -1 Error, -72 to try again (EAGAIN), 0 success.
+	 */
 	virtual int HWTimestamper_txtimestamp
 	( PortIdentity *identity, uint16_t sequenceId, Timestamp &timestamp,
 	  unsigned &clock_value, bool last );
 
+	/**
+	 * @brief  Gets the RX timestamp from the hardware interface. This
+	 * method is not currently in use by the LinuxTimestamperGeneric.
+	 * @param identity Not Used
+	 * @param sequenceId Not Used
+	 * @param timestamp Not Used
+	 * @param clock_value Not Used
+	 * @param last Not Used
+	 * @return -1 error, -72 to try again. 0 Success.
+	 */
 	virtual int HWTimestamper_rxtimestamp
 	( PortIdentity *identity, uint16_t sequenceId, Timestamp &timestamp,
 	  unsigned &clock_value, bool last ) {
@@ -93,8 +158,18 @@ public:
 		return 0;
 	}
 
+	/**
+	 * @brief  Adjusts the clock phase
+	 * @param  phase_adjust Phase adjustment
+	 * @return TRUE if success, FALSE if error.
+	 */
 	virtual bool HWTimestamper_adjclockphase( int64_t phase_adjust );
 	
+	/**
+	 * @brief  Adjusts the frequency
+	 * @param  freq_offset Frequency adjustment
+	 * @return TRUE in case of sucess, FALSE if error.
+	 */
 	virtual bool HWTimestamper_adjclockrate( float freq_offset );
 
 #ifdef WITH_IGBLIB
@@ -102,6 +177,9 @@ public:
 	bool HWTimestamper_PPS_stop();
 #endif
 
+	/*
+	 * deletes LinuxTimestamperGeneric object
+	 */
 	virtual ~LinuxTimestamperGeneric();
 };
 
