4de23ac98d31b978956aacb35d2705113c06e2a0
Rikus Wessels <rikus@umantec.net>
2016-05-19 12:03:32 +0200

    Reformat duplicated pci_connect for easy comparison

--- daemons/gptp/linux/src/linux_hal_i210.cpp
+++ daemons/gptp/linux/src/linux_hal_i210.cpp
@@ -54,50 +54,41 @@ extern "C" {
 static int
 pci_connect( device_t *igb_dev )
 {
-	struct  pci_access      *pacc;
-	struct  pci_dev *dev;
-	int             err;
-	char    devpath[IGB_BIND_NAMESZ];
+	char devpath[IGB_BIND_NAMESZ];
+	struct pci_access *pacc;
+	struct pci_dev *dev;
+	int err;
 
-	memset( igb_dev, 0, sizeof(device_t));
-
-	pacc    =       pci_alloc();
+	memset(igb_dev, 0, sizeof(device_t));
+	pacc = pci_alloc();
 	pci_init(pacc);
 	pci_scan_bus(pacc);
-	for     (dev=pacc->devices;     dev;    dev=dev->next)
-    {
+
+	for (dev = pacc->devices; dev; dev = dev->next) {
 		pci_fill_info(dev, PCI_FILL_IDENT | PCI_FILL_BASES | PCI_FILL_CLASS);
-      
 		igb_dev->pci_vendor_id = dev->vendor_id;
 		igb_dev->pci_device_id = dev->device_id;
 		igb_dev->domain = dev->domain;
 		igb_dev->bus = dev->bus;
 		igb_dev->dev = dev->dev;
 		igb_dev->func = dev->func;
-		
-		snprintf
-			(devpath, IGB_BIND_NAMESZ, "%04x:%02x:%02x.%d", dev->domain,
-			 dev->bus, dev->dev, dev->func );
-
-		err = igb_probe( igb_dev );
-      
+		snprintf(devpath, IGB_BIND_NAMESZ, "%04x:%02x:%02x.%d",
+				dev->domain, dev->bus, dev->dev, dev->func);
+		err = igb_probe(igb_dev);
 		if (err) {
 			continue;
 		}
-
-		printf ("attaching to %s\n", devpath);
-		err = igb_attach( devpath, igb_dev );
-      
+		printf("attaching to %s\n", devpath);
+		err = igb_attach(devpath, igb_dev);
 		if (err) {
-			printf ("attach failed! (%s)\n", strerror(errno));
+			printf("attach failed! (%s)\n", strerror(errno));
 			continue;
 		}
+		/*igb_attach_tx missing here ???*/
 		goto out;
-    }
-
+	}
 	pci_cleanup(pacc);
 	return  ENXIO;
-  
 out:
 	pci_cleanup(pacc);
 	return  0;
--- examples/common/avb.c
+++ examples/common/avb.c
@@ -40,7 +40,7 @@
 
 #include "avb.h"
 
-int pci_connect(device_t * igb_dev)
+int pci_connect(device_t *igb_dev)
 {
 	char devpath[IGB_BIND_NAMESZ];
 	struct pci_access *pacc;
@@ -48,16 +48,12 @@ int pci_connect(device_t * igb_dev)
 	int err;
 
 	memset(igb_dev, 0, sizeof(device_t));
-
 	pacc = pci_alloc();
-
 	pci_init(pacc);
-
 	pci_scan_bus(pacc);
 
 	for (dev = pacc->devices; dev; dev = dev->next) {
-		pci_fill_info(dev,
-				PCI_FILL_IDENT | PCI_FILL_BASES | PCI_FILL_CLASS);
+		pci_fill_info(dev, PCI_FILL_IDENT | PCI_FILL_BASES | PCI_FILL_CLASS);
 		igb_dev->pci_vendor_id = dev->vendor_id;
 		igb_dev->pci_device_id = dev->device_id;
 		igb_dev->domain = dev->domain;
@@ -67,31 +63,27 @@ int pci_connect(device_t * igb_dev)
 		snprintf(devpath, IGB_BIND_NAMESZ, "%04x:%02x:%02x.%d",
 				dev->domain, dev->bus, dev->dev, dev->func);
 		err = igb_probe(igb_dev);
-		if (err)
+		if (err) {
 			continue;
-
+		}
 		printf("attaching to %s\n", devpath);
 		err = igb_attach(devpath, igb_dev);
 		if (err) {
 			printf("attach failed! (%s)\n", strerror(errno));
 			continue;
 		}
-
 		err = igb_attach_tx(igb_dev);
 		if (err) {
 			printf("attach_tx failed! (%s)\n", strerror(errno));
 			igb_detach(igb_dev);
 			continue;
 		}
-
 		goto out;
 	}
-
 	pci_cleanup(pacc);
 	return ENXIO;
-
-out:	pci_cleanup(pacc);
-
+out:
+	pci_cleanup(pacc);
 	return 0;
 }
 
