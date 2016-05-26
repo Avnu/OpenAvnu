b975ddf27c45de81f095285380a01282241d5a22
Rikus Wessels <rikus@umantec.net>
2016-05-26 17:49:04 +0200

    Update linux_hal_i210.cpp

--- daemons/gptp/linux/src/linux_hal_i210.cpp
+++ daemons/gptp/linux/src/linux_hal_i210.cpp
@@ -81,7 +81,7 @@ pci_connect( device_t *igb_dev )
 		printf("attaching to %s\n", devpath);
 		err = igb_attach(devpath, igb_dev);
 		if (err) {
-			printf("attach failed! (%s)\n", strerror(errno));
+			printf("attach failed! (%s)\n", strerror(err));
 			continue;
 		}
 		/*igb_attach_tx missing here ???*/
