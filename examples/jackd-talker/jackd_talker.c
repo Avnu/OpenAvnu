b3876cc8c0eaa9543bbe017ba4d59d5352bc48c8
Rikus Wessels <rikus@umantec.net>
2016-05-27 13:23:51 +0200

    Remove some pci_connect duplicates

--- examples/jackd-talker/jackd_talker.c
+++ examples/jackd-talker/jackd_talker.c
@@ -150,7 +150,8 @@ static inline uint64_t ST_rdtsc(void)
 	return ret;
 }
 
-int gptpinit(int *igb_shm_fd, char **igb_mmap)
+/*gptp seems to be duplicated to ../common/avb too, return values inconsistent*/
+static int gptpinit(int *igb_shm_fd, char **igb_mmap)
 {
 	if (NULL == igb_shm_fd)
 		return -1;
@@ -172,7 +173,7 @@ int gptpinit(int *igb_shm_fd, char **igb_mmap)
 	return 0;
 }
 
-int gptpdeinit(int *igb_shm_fd, char **igb_mmap)
+static int gptpdeinit(int *igb_shm_fd, char **igb_mmap)
 {
 	if (NULL == igb_shm_fd)
 		return -1;
@@ -189,7 +190,7 @@ int gptpdeinit(int *igb_shm_fd, char **igb_mmap)
 	return 0;
 }
 
-int gptpscaling(char *igb_mmap, gPtpTimeData *td)
+static int gptpscaling(char *igb_mmap, gPtpTimeData *td)
 {
 	if (NULL == td)
 		return -1;
@@ -211,53 +212,6 @@ void sigint_handler(int signum)
 	printf("got SIGINT\n");
 	*halt_tx_sig = signum;
 	glob_unleash_jack = 0;
-}
-
-int pci_connect(device_t *igb_dev)
-{
-	char devpath[IGB_BIND_NAMESZ];
-	struct pci_access *pacc;
-	struct pci_dev *dev;
-	int err;
-
-	memset(igb_dev, 0, sizeof(device_t));
-	pacc = pci_alloc();
-	pci_init(pacc);
-	pci_scan_bus(pacc);
-
-	for (dev = pacc->devices; dev; dev = dev->next) {
-		pci_fill_info(dev, PCI_FILL_IDENT | PCI_FILL_BASES | PCI_FILL_CLASS);
-		igb_dev->pci_vendor_id = dev->vendor_id;
-		igb_dev->pci_device_id = dev->device_id;
-		igb_dev->domain = dev->domain;
-		igb_dev->bus = dev->bus;
-		igb_dev->dev = dev->dev;
-		igb_dev->func = dev->func;
-		snprintf(devpath, IGB_BIND_NAMESZ, "%04x:%02x:%02x.%d",
-				dev->domain, dev->bus, dev->dev, dev->func);
-		err = igb_probe(igb_dev);
-		if (err) {
-			continue;
-		}
-		printf("attaching to %s\n", devpath);
-		err = igb_attach(devpath, igb_dev);
-		if (err) {
-			printf("attach failed! (%s)\n", strerror(err));
-			continue;
-		}
-		err = igb_attach_tx(igb_dev);
-		if (err) {
-			printf("attach_tx failed! (%s)\n", strerror(err));
-			igb_detach(igb_dev);
-			continue;
-		}
-		goto out;
-	}
-	pci_cleanup(pacc);
-	return ENXIO;
-out:
-	pci_cleanup(pacc);
-	return 0;
 }
 
 int get_mac_address(char *interface)
--- examples/simple_talker/simple_talker.c
+++ examples/simple_talker/simple_talker.c
@@ -257,7 +257,7 @@ static inline uint64_t ST_rdtsc(void)
 	return ret;
 }
 
-int gptpinit(int *igb_shm_fd, char **igb_mmap)
+static int gptpinit(int *igb_shm_fd, char **igb_mmap)
 {
 	if (NULL == igb_shm_fd)
 		return -1;
@@ -279,7 +279,7 @@ int gptpinit(int *igb_shm_fd, char **igb_mmap)
 	return 0;
 }
 
-int gptpdeinit(int *igb_shm_fd, char **igb_mmap)
+static int gptpdeinit(int *igb_shm_fd, char **igb_mmap)
 {
 	if (NULL == igb_shm_fd)
 		return -1;
@@ -296,7 +296,7 @@ int gptpdeinit(int *igb_shm_fd, char **igb_mmap)
 	return 0;
 }
 
-int gptpscaling(const char *igb_mmap, gPtpTimeData *td)
+static int gptpscaling(const char *igb_mmap, gPtpTimeData *td)
 {
 	if (NULL == td)
 		return -1;
@@ -354,53 +354,6 @@ void sigint_handler(int signum)
 {
 	printf("got SIGINT\n");
 	*halt_tx_sig = signum;
-}
-
-int pci_connect(device_t *igb_dev)
-{
-	char devpath[IGB_BIND_NAMESZ];
-	struct pci_access *pacc;
-	struct pci_dev *dev;
-	int err;
-
-	memset(igb_dev, 0, sizeof(device_t));
-	pacc = pci_alloc();
-	pci_init(pacc);
-	pci_scan_bus(pacc);
-
-	for (dev = pacc->devices; dev; dev = dev->next) {
-		pci_fill_info(dev, PCI_FILL_IDENT | PCI_FILL_BASES | PCI_FILL_CLASS);
-		igb_dev->pci_vendor_id = dev->vendor_id;
-		igb_dev->pci_device_id = dev->device_id;
-		igb_dev->domain = dev->domain;
-		igb_dev->bus = dev->bus;
-		igb_dev->dev = dev->dev;
-		igb_dev->func = dev->func;
-		snprintf(devpath, IGB_BIND_NAMESZ, "%04x:%02x:%02x.%d",
-				dev->domain, dev->bus, dev->dev, dev->func);
-		err = igb_probe(igb_dev);
-		if (err) {
-			continue;
-		}
-		printf("attaching to %s\n", devpath);
-		err = igb_attach(devpath, igb_dev);
-		if (err) {
-			printf("attach failed! (%s)\n", strerror(err));
-			continue;
-		}
-		err = igb_attach_tx(igb_dev);
-		if (err) {
-			printf("attach_tx failed! (%s)\n", strerror(err));
-			igb_detach(igb_dev);
-			continue;
-		}
-		goto out;
-	}
-	pci_cleanup(pacc);
-	return ENXIO;
-out:
-	pci_cleanup(pacc);
-	return 0;
 }
 
 void l3_to_l2_multicast( unsigned char *l2, unsigned char *l3 ) {
