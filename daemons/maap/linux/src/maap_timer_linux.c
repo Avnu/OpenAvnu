a4dd3e582fad61dba8e74d8b8b68b3d6cc9c99eb
Brant Thomsen <brant.thomsen@harman.com>
2016-09-30 15:10:30 -0600

    Updates so that the main loop delays for, and handles the timers.
    Cleaned up the stdin command parsing.

--- daemons/maap/linux/src/maap_timer_linux.c
+++ daemons/maap/linux/src/maap_timer_linux.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <assert.h>
 #include <time.h>
+#include <errno.h>
 
 struct maap_timer {
   timer_t timer_id;
@@ -12,12 +14,28 @@ struct maap_timer {
 
 Timer *Time_newTimer(void)
 {
-  return malloc(sizeof (Timer));
+  struct sigevent sev;
+  Timer * newTimer = malloc(sizeof (Timer));
+  if (newTimer)
+  {
+    sev.sigev_notify = SIGEV_NONE;
+    sev.sigev_signo = 0;
+    sev.sigev_value.sival_ptr = NULL;
+    if (timer_create(CLOCK_MONOTONIC, &sev, &(newTimer->timer_id)) < 0)
+    {
+      newTimer->timer_id = (timer_t)(-1);
+    }
+  }
+  return newTimer;
 }
 
 void Time_delTimer(Timer *timer)
 {
   assert(timer);
+  if (timer && timer->timer_id != (timer_t)(-1))
+  {
+    timer_delete(timer->timer_id);
+  }
   free(timer);
 }
 
@@ -30,6 +48,21 @@ void Time_setTimer(Timer *timer, Time *t)
   tspec.it_interval.tv_nsec = 0;
   timer_settime(timer->timer_id, TIMER_ABSTIME, &tspec, NULL);
 }
+
+int64_t Time_remaining(Timer *timer)
+{
+	struct itimerspec curr_value;
+
+	assert(timer);
+	if (timer_gettime(timer->timer_id, &curr_value) < 0)
+	{
+		printf("Error %d getting the timer time remaining\n", errno);
+		return -1;
+	}
+
+	return (curr_value.it_value.tv_sec * 1000000000L + curr_value.it_value.tv_nsec);
+}
+
 
 void Time_add(Time *a, Time *b)
 {
