87f3a68656e483b64cbb67ae5452e96b573e629b
Brant Thomsen <brant.thomsen@harman.com>
2017-08-01 14:56:23 -0600

    Spelling and whitespace fixes

--- lib/avtp_pipeline/map_uncmp_audio/openavb_map_uncmp_audio_pub.h
+++ lib/avtp_pipeline/map_uncmp_audio/openavb_map_uncmp_audio_pub.h
@@ -2,16 +2,16 @@
 Copyright (c) 2012-2015, Symphony Teleca Corporation, a Harman International Industries, Incorporated company
 Copyright (c) 2016-2017, Harman International Industries, Incorporated
 All rights reserved.
- 
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
- 
+
 1. Redistributions of source code must retain the above copyright notice, this
    list of conditions and the following disclaimer.
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
    and/or other materials provided with the distribution.
- 
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS LISTED "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,20 +22,20 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
-Attributions: The inih library portion of the source code is licensed from 
-Brush Technology and Ben Hoyt - Copyright (c) 2009, Brush Technology and Copyright (c) 2009, Ben Hoyt. 
-Complete license and copyright information can be found at 
+
+Attributions: The inih library portion of the source code is licensed from
+Brush Technology and Ben Hoyt - Copyright (c) 2009, Brush Technology and Copyright (c) 2009, Ben Hoyt.
+Complete license and copyright information can be found at
 https://github.com/benhoyt/inih/commit/74d2ca064fb293bc60a77b0bd068075b293cf175.
 *************************************************************************************************************/
 
 /*
 * HEADER SUMMARY : Uncompressed Audio mapping module public interface
-* 
+*
 * Refer to IEC 61883-6 for details of the "source packet" structure.
-* 
+*
 * The protocol_specific_header and CIP header.
-* 
+*
 * map_nv_tx_rate must be set in the .ini file.
 */
 
@@ -122,12 +122,12 @@ typedef struct {
 	/// synchronization time interval
 	U32 sytInterval;
 
-	/// CB for interface modules to do translations in place before data is moved into the mediaQ on rx.	
+	/// CB for interface modules to do translations in place before data is moved into the mediaQ on rx.
 	openavb_intf_rx_translate_cb_t	intf_rx_translate_cb;
 
 	/// Interface Module may set this presentation latency which listener mapping modules will use to adjust the presetnation time
 	S32 presentationLatencyUSec;
-	
+
 } media_q_pub_map_uncmp_audio_info_t;
 
 #endif  // OPENAVB_MAP_UNCMP_AUDIO_PUB_H
