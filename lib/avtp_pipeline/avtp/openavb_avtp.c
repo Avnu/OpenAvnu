e836d26f4576ce45f828e4357e835f73999eb1b8
Brant Thomsen <brant.thomsen@harman.com>
2017-08-21 15:50:33 -0600

    Removed trailing whitespace

--- lib/avtp_pipeline/avtp/openavb_avtp.c
+++ lib/avtp_pipeline/avtp/openavb_avtp.c
@@ -2,16 +2,16 @@
 Copyright (c) 2012-2015, Symphony Teleca Corporation, a Harman International Industries, Incorporated company
 Copyright (c) 2016-2017, Harman International Industries, Incorporated
 All rights reserved.
- 
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
- 
+
 1. Redistributions of source code must retain the above copyright notice, this
    list of conditions and the following disclaimer.
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
    and/or other materials provided with the distribution.
- 
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS LISTED "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,10 +22,10 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
-Attributions: The inih library portion of the source code is licensed from 
-Brush Technology and Ben Hoyt - Copyright (c) 2009, Brush Technology and Copyright (c) 2009, Ben Hoyt. 
-Complete license and copyright information can be found at 
+
+Attributions: The inih library portion of the source code is licensed from
+Brush Technology and Ben Hoyt - Copyright (c) 2009, Brush Technology and Copyright (c) 2009, Ben Hoyt.
+Complete license and copyright information can be found at
 https://github.com/benhoyt/inih/commit/74d2ca064fb293bc60a77b0bd068075b293cf175.
 *************************************************************************************************************/
 
@@ -74,7 +74,7 @@ static openavbRC openAvtpSock(avtp_stream_t *pStream)
 
 	if (pStream->rawsock != NULL) {
 		openavbSetRxSignalMode(pStream->rawsock, pStream->bRxSignalMode);
-		
+
 		if (!pStream->tx) {
 			// Set the multicast address that we want to receive
 			openavbRawsockRxMulticast(pStream->rawsock, TRUE, pStream->dest_addr.ether_addr_octet);
@@ -256,7 +256,7 @@ static void inline rxDeliveryStats(avtp_rx_info_t *rxInfo,
 static openavbRC fillAvtpHdr(avtp_stream_t *pStream, U8 *pFill)
 {
 	AVB_TRACE_ENTRY(AVB_TRACE_AVTP_DETAIL);
-  
+
 	switch (pStream->pMapCB->map_avtp_version_cb()) {
 		default:
 			AVB_RC_LOG_RET(AVB_RC(OPENAVB_AVTP_FAILURE | OPENAVBAVTP_RC_INVALID_AVTP_VERSION));
@@ -342,7 +342,7 @@ openavbRC openavbAvtpTx(void *pv, bool bSend, bool txBlockingInIntf)
 
 			// Call mapping module to move data into AVTP frame
 			txCBResult = pStream->pMapCB->map_tx_cb(pStream->pMediaQ, pAvtpFrame, &avtpFrameLen);
-		
+
 			pStream->bytes += avtpFrameLen;
 		}
 		else {
@@ -515,7 +515,7 @@ static void x_avtpRxFrame(avtp_stream_t *pStream, U8 *pFrame, U32 frameLen)
 			}
 
 			pStream->pMapCB->map_rx_cb(pStream->pMediaQ, pFrame, frameLen);
-			
+
 			// NOTE : This is a redundant call. It is handled in avtpTryRx()
 			// pStream->pIntfCB->intf_rx_cb(pStream->pMediaQ);
 
@@ -577,7 +577,7 @@ static void avtpTryRx(avtp_stream_t *pStream)
 				timeout = AVTP_MAX_BLOCK_USEC;
 			if (timeout < RAWSOCK_MIN_TIMEOUT_USEC)
 				timeout = RAWSOCK_MIN_TIMEOUT_USEC;
-			
+
 			pBuf = (U8 *)openavbRawsockGetRxFrame(pStream->rawsock, timeout, &offsetToFrame, &frameLen);
 			if (!pBuf)
 				pStream->pIntfCB->intf_rx_cb(pStream->pMediaQ);
